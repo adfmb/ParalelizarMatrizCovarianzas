f3b7b9bbe109fd9e12ad2ce0bd9ba43ea715c2b9
ehz500 <elisa.hdz@icloud.com>
2016-05-22 00:44:25 -0500

    f1 for rows and columns

---
+++ f1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main (void){
+
+    int m,p,q;
+    p = 0;
+    q=0;
+    //int arreglo[2]=0;
+    
+    m = getchar();
+    while( (m = getchar()) != EOF){
+        if(m=='\n')
+        p=p+1;
+        
+        
+        if(m==',')
+        q=q+1;
+        
+        
+    }
+    q=(q+p+1)/(p+1);
+    p=p+1;
+    printf("%d\n %d\n",p, q);
+    
+return 0;
+    
+}
